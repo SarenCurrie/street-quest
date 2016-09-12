e4f415e0db9d5291cbd0cb43668f895cad94f68f
ladiespls <fsta657@aucklanduni.ac.nz>
2016-09-12 20:43:28 +1200

    Ironed out some nasty display text bugs.

--- js/questManage.js
+++ js/questManage.js
@@ -10,7 +10,7 @@ function checkPosition(questlog, markerToCheck) {
           && markerToCheck.getPosition().lat() == quest.positions[i].getPosition().lat()){
         // we are in a quest, do that action
         // check what the progress is for this quest
-        console.log('i got here and everything was ok');
+        console.log('Matching point found.');
         // first things first, if any of the points we need to go to haven't been made, make them right now! ahh!
         if (quest.action[i].completed != true){ // these should be changed into something that's not boolean comparisons lol
         for (var j = 0; j < quest.action[i].to_visit[quest.action[i].progress].length; j++){
@@ -40,14 +40,16 @@ function checkPosition(questlog, markerToCheck) {
         // then, check if we've been here before
           if (quest.action[i].visited != true){
               quest.action[i].visited = true;
+              console.log('This should print the introduction.');
               ui.makeDialog(quest.action[i].npcName, quest.action[i].intro_dialog);
             // if this is the first time we've been here, see if anything else needs doing
-            if (quest.action[i].to_visit.length == 0){
+            if (quest.action[i].to_visit.length == 1 && quest.action[i].to_visit[0].length == 0){
               quest.action[i].completed = true;
-
+              quest.action[i].progress += 1;
             }
             // if we've been here, and we've finished here, then say that's the case, using the u r done dialog
           } else if (quest.action[i].completed == true){
+              console.log('This should print the final text.');
               ui.makeDialog(quest.action[i].npcName, quest.action[i].completion_dialog[quest.action[i].progress]);
           }
           else{
@@ -55,16 +57,21 @@ function checkPosition(questlog, markerToCheck) {
           for (var j = 0; j < quest.action[i].to_visit[quest.action[i].progress].length; j++){
             // check if we have been to a place, if not, say something about going there or whatever
             if (quest.action[quest.action[i].to_visit[quest.action[i].progress][j]].visited == false){
+              console.log('This text shows if a location has not been visited.')
               ui.makeDialog(quest.action[i].npcName, quest.action[i].been_here_not_visited_dialog[quest.action[i].progress][j]);
               return;
               // now check if we've been there, but havan't finished the task they gave
-            } else if (quest.action[quest.action[i].to_visit[quest.action[i].progress][j]].completed == false) {
+            } }
+            for (var j = 0; j < quest.action[i].to_visit[quest.action[i].progress].length; j++){
+            if (quest.action[quest.action[i].to_visit[quest.action[i].progress][j]].completed == false) {
+              console.log('This text shows if a location has not had the task completed.');
               ui.makeDialog(quest.action[i].npcName, quest.action[i].been_here_not_completed_dialog[quest.action[i].progress][j]);
               return;
             }
           }
           // if none of these returned, we've actually completed the allocated task things, good job!
           // print the completion dialog for this section, give the reward, delete relevant points, and increment the progress for this node
+          console.log('This text indicates completion, and should transfer into the next phase.');
           ui.makeDialog(quest.action[i].npcName, quest.action[i].completion_dialog[quest.action[i].progress]);
           quest.action[i].progress += 1;
           // if there is no more places to go, we've completed this segement
